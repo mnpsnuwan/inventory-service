716308e2a87d38e3977ee303e8b538e1c3321d8c
mnpsnuwan <nuwanp124@gmail.com>
2024-08-14 20:23:24 -0700

    IM:DEV:Inventory Service - Setting up REST API swagger documentation with Open API.

---
+++ src/main/java/com/imatrix/microservices/inventory/config/OpenAPIConfig.java
@@ -0,0 +1,33 @@
+package com.imatrix.microservices.inventory.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : InventoryService
+ * Class : {@link OpenAPIConfig}
+ *
+ * @author : Nuwan Samarasinghe
+ */
+
+@Configuration
+public class OpenAPIConfig
+{
+    @Bean
+    public OpenAPI inventoryServiceAPI()
+    {
+        return new OpenAPI()
+                       .info( new Info().title( "Inventory Service API" )
+                                        .description( "This is the REST API for Inventory Service" )
+                                        .version( "v0.0.1" )
+                                        .license( new License().name( "Apache 2.0" ) ) )
+                       .externalDocs( new ExternalDocumentation()
+                                              .description( "You can refer to the Inventory Service Wiki Documentation" )
+                                              .url( "https://inventory-service-dummy-url.com/docs" ) );
+    }
+}
